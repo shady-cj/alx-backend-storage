73ce3f034555d9c1e4495e4efe3f3e005e8008d3
peter <petersp2000@gmail.com>
2023-04-08 08:06:44 +0100

    implemented task #5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,11 +3,9 @@
 DELIMITER $$ ;
 CREATE TRIGGER validate_email BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-IF NEW.email NOT LIKE '%_@__%.__%' AND PATINDEX('%[^a-z,0-9,@,.,_,\-]%', NEW.email) <> 0
-THEN
+IF NEW.email NOT LIKE '%_@__%.__%' OR NEW.email LIKE '%+%' THEN
 SET NEW.valid_email = 0;
-ELSE
-SET NEW.valid_email = 1;
+ELSE SET NEW.valid_email = 1;
 END IF;
 END $$
 DELIMITER ; $$
