3315406615895874d2cfe6c3afca6165b7155308
shady-cj <petersp2000@gmail.com>
2023-04-13 16:41:03 +0100

    update on 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,3 +2,11 @@
 """
 a Python script that provides some stats about Nginx logs stored in MongoDB:
 """
+from pymongo import import MongoClient
+
+
+def main():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    x_logs = nginx.find().count()
+    aggregate_logs = nginx.aggregate([{"$group": {"method": "$method", "num_of_occurence": {"$sum": 1}}}])
