9730557816d268698f84261f14b8c85090a57471
peter <petersp2000@gmail.com>
2023-04-13 15:46:59 +0100

    implemented task #10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A Python function that changes all topics of a school document based
+on the name:
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    updates the mongo_collection with the name of arg name and an
+    attribute topics
+    """
+
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
