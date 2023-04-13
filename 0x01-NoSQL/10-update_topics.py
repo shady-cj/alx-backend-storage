bf3fbfe22f68c5cb63c2b7b8e0afec3627896809
peter <petersp2000@gmail.com>
2023-04-13 15:49:32 +0100

    implemented task #10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -11,4 +11,4 @@ def update_topics(mongo_collection, name, topics):
     attribute topics
     """
 
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
