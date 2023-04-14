a68838c7def2cc29cff36b881c252653496a196e
peter <petersp2000@gmail.com>
2023-04-14 06:10:48 +0100

    implemented task #12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,11 +2,34 @@
 """
 a Python script that provides some stats about Nginx logs stored in MongoDB:
 """
-from pymongo import import MongoClient
+from pymongo import MongoClient
 
 
 def main():
+    """
+    prints the stats from the nginx logs
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx = client.logs.nginx
-    x_logs = nginx.find().count()
-    aggregate_logs = nginx.aggregate([{"$group": {"method": "$method", "num_of_occurence": {"$sum": 1}}}])
+    x_logs = nginx.count_documents({})
+    aggregate_logs = nginx.aggregate([{"$group": {"_id": "$method", "num_of_occurence": {"$sum": 1}}}])
+    methods = {}
+    for m in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        for m_log in aggregate_logs:
+            if m == m_log.get('_id'):
+                methods[m] = m_log.get('num_of_occurence')
+                break
+        if methods.get(m):
+            continue
+        methods[m] = 0
+
+    print(f"{x_logs} logs")
+    print("Methods:")
+    for meth in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"\tmethod {meth}: {methods.get(meth)}")
+    stat_check = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{stat_check} status check")
+
+
+if __name__ == "__main__":
+    main()
