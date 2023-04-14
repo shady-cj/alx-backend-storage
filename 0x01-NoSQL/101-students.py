ba930dcd22fca25536edd0f8cf5d1a7f6f239a77
peter <petersp2000@gmail.com>
2023-04-14 07:35:41 +0100

    implemented task#12

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a Python function that returns all students sorted by average score:
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Using aggregation to find the average score of student
+    """
+    collection = mongo_collection.aggregate(
+        [
+            {"$unwind": "$topics"},
+            {"$group":
+                {
+                    "_id": "$name",
+                    "id": {"$first": "$_id"},
+                    "name": {"$first": "$name"},
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+             },
+            {"$sort": {"averageScore": -1}},
+            {"$project": {
+                            "_id": "$id",
+                            "name": "$name",
+                            "averageScore": "$averageScore"
+                        }
+             }
+        ])
+    return collection
