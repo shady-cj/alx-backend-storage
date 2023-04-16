ca008c89879088539f89a415c7f3bfac360438b2
peter <petersp2000@gmail.com>
2023-04-16 06:28:45 +0100

    implement task #5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+implementing a get_page
+function (prototype: def get_page(url: str) -> str:)
+"""
+import requests
+import redis
+import typing
+import functools
+
+
+red = redis.Redis()
+red.flushdb()
+
+
+def count_url(fn: typing.Callable) -> typing.Callable:
+    """
+    decorator to get the number of times url was used in
+    get_page
+    """
+    @functools.wraps(fn)
+    def count(url):
+        if red.exists(f"count:{url}"):
+            red.incr(f"count:{url}")
+        else:
+            red.setex(f"count:{url}", 10, 1)
+        return fn(url)
+    return count
+
+
+@count_url
+def get_page(url: str) -> str:
+    """
+    getting the page and caching the url for 10 seconds
+    """
+    ret = requests.get(url)
+    return ret.content
+
+# get_page('https://www.google.com')
+# get_page('https://www.google.com')
+# get_page('https://www.google.com')
+# print(red.get("count:https://www.google.com"))
+# import time
+# time.sleep(10)
+# get_page('https://www.google.com')
+# get_page('https://www.google.com')
+# print(red.get("count:https://www.google.com"))
